c47ecaa0b78eeffa642ea05ffa5ea922f8be9a10
BeastPony <clyde.filippov@gmail.com>
2025-12-15 16:47:01 +0300

    new commit

--- test_demo.py
+++ test_demo.py
@@ -1,7 +1,19 @@
 import string
 import random
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
+@pytest.fixture()
+def before_after():
+    print('Before test')
+
+def test_demo1():
+    assert 1 == 1
+
+
+def test_demo2():
+    assert 2 == 2
 
